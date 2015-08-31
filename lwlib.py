1c534c131a456f7619a9ceca59205573be93ab14
Bhaskar Agarwal <a.bhaskar.is@gmail.com>
2015-08-31 13:59:04 -0400

    new changes

--- lwlib.py
+++ lwlib.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import integrate
 import ba_constants as bac
+import pandas as pd
 
 def computerates(SED, wvlt, pos, z_current):
     """
@@ -71,3 +72,48 @@ def computerates(SED, wvlt, pos, z_current):
     kde = kappa_de * alpha * jlw/dist**2 * bac.c21
 
     return kde, kdi, jlw
+
+def sedcompute(mass, age, pos):
+
+    file_path = '/Users/BAMAC/GitStuff/BaAstro/Erik/'
+    file_name = file_path + 'SED_2'
+    size = 54 # tied to file name, check if you change filename
+
+    age_index = [str(x) for x in range(0,size)]
+    sed_data = pd.read_csv(file_name,delim_whitespace=True,names=age_index,skiprows=1)
+
+    age_data = []
+    age_data = [np.append(age_data,sed_data[str(x)][0]) for x in range(0,size)]
+    age_data_log = np.log10(age_data)
+
+    wvlt_file = file_path+'wvlt_A_Erik'
+    wvlt_data = pd.read_csv(file_path,names='A',skiprows=1) # wvlt in angstroms
+    wvlt_A = np.array(wvlt_data['A'])
+
+    n_gal = len(pos)
+
+    # define the LW parameters that you will use
+    kde = 0.
+    kdi =0.
+    jlw =0.
+
+    # each galaxy here
+    for gi in range(0,n_gal):
+
+        # you could modify it to store an SED for *each* of the galaxies, but i dont see a point
+        SED_gal = 0.
+
+        for gi_i in range(0,len(mass[gi])):
+            agetomatch = np.log10(age[gi][gi_i])
+            age_match_id = min(enumerate(age_data_log), key=lambda age_log: abs(age_data_log[1] - agetomatch))
+
+            SED_gal += sed_data[str(int(age_match_id[0]))][1:]*mass[gi][gi_i]/1e6
+
+        # pass this SED to the alpha/beta/jlw module
+        lw_quantities = np.array(computerates(SED_gal,wvlt_A), pos[gi]) # check the pos bit
+        kde += lw_quantities[0]
+        kdi += lw_quantities[1]
+        jlw += lw_quantities[2]
+
+    return kde,kdi,jlw
+
